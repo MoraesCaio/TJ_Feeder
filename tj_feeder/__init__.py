d71ed9e49b6dce579e9add6bb7c721a1f459ec0e
MoraesCaio <caiomoraes.cesar@gmail.com>
2022-01-09 21:03:54 -0300

    Linted code

--- tj_feeder/__init__.py
+++ tj_feeder/__init__.py
@@ -1,9 +1,15 @@
+"""TJ Feed module
+"""
+
 from pathlib import Path
+from typing import Union
 
-
-CFG_FILE = Path(__file__).parent / 'data' / 'cfg.json'
+T_NUMBER = Union[int, float]
+T_PATH = Union[Path, str]
+T_LOG_LEVEL = Union[int, str]
+CFG_FILE = Path(__file__).parent / "data" / "cfg.json"
 HEADERS = [
-    'time_spent',
-    'issue_name',
-    'issue_description',
+    "time_spent",
+    "issue_name",
+    "issue_description",
 ]
--- tj_feeder/configs.py
+++ tj_feeder/configs.py
@@ -1,21 +1,33 @@
+"""Module to load and save the config json file."""
+
 import json
 
-from tj_feeder import CFG_FILE
+from tj_feeder import CFG_FILE, T_PATH
 
 
-def load(cfg_file: str = CFG_FILE) -> dict:
+def load(cfg_file: T_PATH = CFG_FILE) -> dict:
     """Loads default settings
 
-    Returns:
-        dict: Default settings
+    Args:
+        cfg_file (T_PATH, optional): Path to config json file. Defaults to
+            CFG_FILE.
+
+    Returns: dict: Default settings
     """
-    with open(CFG_FILE, 'r') as cfg_file:
-        settings = json.load(cfg_file)
+    with open(cfg_file, "r") as configs:
+        settings = json.load(configs)
 
     return settings
 
 
-def save(cfg_dict: dict, cfg_file: str = CFG_FILE) -> None:
-    print(f'Saving default settings...')
-    with open(cfg_file, 'w') as cfg_json:
+def save(cfg_dict: dict, cfg_file: T_PATH = CFG_FILE) -> None:
+    """Save config json file
+
+    Args:
+        cfg_dict (dict): Dictionary with new default settings
+        cfg_file (T_PATH, optional): Path to config json file. Defaults to
+            CFG_FILE.
+    """
+    print("Saving default settings...")
+    with open(cfg_file, "w") as cfg_json:
         json.dump(cfg_dict, cfg_json, indent=4)
